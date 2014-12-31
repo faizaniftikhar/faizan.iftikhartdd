a70343489b8d96aaff5e4aac61318d501e98e239
Faizan Iftikhar <Faizan Iftikhar@FaizanIftikhar>
2014-12-31 23:45:05 +0500

    1st Test case(DollarTest) before to start code Chapter 1

---
+++ src/com/sibisoft/faizaniftikhartdd/DollarTest.java
@@ -0,0 +1,19 @@
+package com.sibisoft.faizaniftikhartdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DollarTest 
+{
+	@Test
+	public void testMultiplication()
+	{
+		Dollar dollar = new Dollar(5);
+		dollar.times(2);
+		assertEquals(10, five.amount);
+	}
+	
+	
+
+}
