8ef5f3213741df6c49089257bd10cdf997863e9c
Faizan Iftikhar <Faizan Iftikhar@FaizanIftikhar>
2015-01-01 16:34:05 +0500

    To make test run get new object of Dollar from each object

--- src/com/sibisoft/faizaniftikhartdd/DollarTestV2.java
+++ src/com/sibisoft/faizaniftikhartdd/DollarTestV2.java
@@ -11,9 +11,9 @@ public class DollarTestV2
 	public void testMultiplication() 
 	{
 			Dollar five= new Dollar(5);
-			five.times(2);
-			assertEquals(10, five.amount);
-			five.times(3);
-			assertEquals(15, five.amount);
+			Dollar container1 = five.times(2);
+			assertEquals(10, container1.amount);
+			Dollar container2 = five.times(3);
+			assertEquals(15, container2.amount);
 	}
 }
