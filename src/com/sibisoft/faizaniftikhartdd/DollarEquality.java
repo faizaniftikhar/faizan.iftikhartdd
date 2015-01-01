78d3f89d1b313ff6852c4a2103a50e7e9192795e
Faizan Iftikhar <Faizan Iftikhar@FaizanIftikhar>
2015-01-01 16:40:01 +0500

    Add new test case for equal method(Fake Implementation) to make red bar

---
+++ src/com/sibisoft/faizaniftikhartdd/DollarEquality.java
@@ -0,0 +1,15 @@
+package com.sibisoft.faizaniftikhartdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DollarEquality {
+
+	@Test
+	public void testEquality() 
+	{
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+	}
+
+}
