c83c43ded8a1993ed0cf2198d9295893a3c06617
Faizan Iftikhar <Faizan Iftikhar@FaizanIftikhar>
2015-01-02 17:47:55 +0500

    Change Franc cast into Money

--- src/com/sibisoft/faizaniftikhartdd/Franc.java
+++ src/com/sibisoft/faizaniftikhartdd/Franc.java
@@ -16,7 +16,7 @@ public class Franc extends Money
 	}
 	public boolean equals(Object object) 
 	{
-		Money franc= (Franc) object;
+		Money franc= (Money) object;
 		return amount == franc.amount;
 	}
 
