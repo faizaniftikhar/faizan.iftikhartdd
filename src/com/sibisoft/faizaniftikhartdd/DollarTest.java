594a79b4e02457e5e3f1b21a9f133e691e4e86bf
Faizan Iftikhar <Faizan Iftikhar@FaizanIftikhar>
2015-01-02 16:49:39 +0500

    Include all test cases in one test class

--- src/com/sibisoft/faizaniftikhartdd/DollarTest.java
+++ src/com/sibisoft/faizaniftikhartdd/DollarTest.java
@@ -7,13 +7,23 @@ import org.junit.Test;
 public class DollarTest 
 {
 	@Test
-	public void testMultiplication()
+	public void testMultiplication() 
 	{
-		Dollar dollar = new Dollar(5);
-		dollar.times(3);
-		assertEquals(10, dollar.amount);
+			Dollar five= new Dollar(5);
+			assertEquals(new Dollar(10), five.times(2));
+			assertEquals(new Dollar(15), five.times(3));
 	}
-	
-	
-
+	@Test
+	public void testFrancMultiplication() 
+	{
+		Franc five= new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
+	}
+	@Test
+	public void testEquality() 
+	{
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
+	}
 }
